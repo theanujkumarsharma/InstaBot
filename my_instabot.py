b6e335b1f3720462360e15665ea841fe3c160de0
A. K. Sharma <124619135+theanujkumarsharma@users.noreply.github.com>
2025-04-18 21:56:04 +0530

    my_instabot.py

--- my_instabot.py
+++ my_instabot.py
@@ -1,13 +1,13 @@
 from instabot import Bot
 
 bot = Bot()
-bot.login(username="astra.sha01", password="Sharma#12564")
+bot.login(username="your_username", password="your_password")
 
 # Follow a user
-bot.follow("the_a_k_sharma")
+bot.follow("username")
 
 # Like and comment on a user's most recent post
-user_id = bot.get_user_id_from_username("the_a_k_sharma")
+user_id = bot.get_user_id_from_username("username")
 media_ids = bot.get_user_medias(user_id, filtration=False)
 
 if media_ids:
@@ -15,4 +15,4 @@ if media_ids:
     bot.comment(media_ids[0], "Great post!")
 
 # Try sending a DM
-bot.send_message("Hello from my Python bot!", ["the_a_k_sharma"])
+bot.send_message("Hello from my Python bot!", ["username"])
